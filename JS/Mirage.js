a63e0d550c7f367dc1bdfde925119230005ff1ce
dryAgedBanana <b.cao@sintlucasedu.nl>
2023-10-10 21:36:10 +0200

    Update Mirage

--- JS/Mirage.js
+++ JS/Mirage.js
@@ -48,3 +48,40 @@ function mirageScroll() {
         exploreContent.classList.add('fade-in');
     }
 }
+
+document.getElementById('uk-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior (i.e., following the link)
+
+    // Toggle the display property of the dropdown content
+    const dropdownContent = document.querySelector('.language-dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const languageSelector = document.getElementById('languageSelector');
+
+    // Event listener to handle clicks on the body
+    document.body.addEventListener('click', function(event) {
+        const languageDropdown = languageSelector.querySelector('.language-dropdown-content');
+
+        if (languageDropdown.style.display === 'block') {
+            languageDropdown.style.display = 'none';
+        }
+    });
+
+    // Event listener to prevent clicks inside the language selector from hiding the dropdown
+    languageSelector.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+
+    // Event listeners for hover effect
+    languageSelector.addEventListener('mouseover', function() {
+        const languageDropdown = languageSelector.querySelector('.language-dropdown-content');
+        languageDropdown.style.display = 'block';
+    });
+
+    languageSelector.addEventListener('mouseout', function() {
+        const languageDropdown = languageSelector.querySelector('.language-dropdown-content');
+        languageDropdown.style.display = 'none';
+    });
+});
