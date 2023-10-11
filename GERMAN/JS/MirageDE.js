9bbf288b0a9825fc07e16df4c13e91adc96f49f5
dryAgedBanana <b.cao@sintlucasedu.nl>
2023-10-11 20:37:27 +0200

    Patching menu dropdown

--- GERMAN/JS/MirageDE.js
+++ GERMAN/JS/MirageDE.js
@@ -59,7 +59,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-document.getElementById('uk-link').addEventListener('click', function(event) {
+document.getElementById('de-link').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default behavior (i.e., following the link)
 
     // Toggle the display property of the dropdown content
